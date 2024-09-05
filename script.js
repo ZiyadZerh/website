dee150dd0aca0c84b144f214b6dab8782dbc9d18
ZiyadZerh <71100081+ZiyadZerh@users.noreply.github.com>
2024-09-05 20:59:36 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -2,5 +2,4 @@
 document.addEventListener('DOMContentLoaded', function() {
 var element = document.getElementsByClassName('Github')[0];
 element.addEventListener('click', function() {
-window.open('https://github.com/Ziyadzerh', '_blank');});
-});
+window.open('https://github.com/Ziyadzerh', '_blank');});});
